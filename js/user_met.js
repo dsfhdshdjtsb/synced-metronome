dae3fd4f1ac80e783374328ac807b7a17ab09175
Nicholas Suh <nicksuh17@gmail.com>
2022-05-02 20:48:31 -0700

    cool shid

--- js/user_met.js
+++ js/user_met.js
@@ -3,11 +3,14 @@ import Timer from './timer.js';
 const tempoDisplay = document.querySelector('.tempo');
 const tempoText = document.querySelector('.tempo-text');
 const measureCount = document.querySelector('.measure-count');
+const testButton = document.querySelector('.testButton');
 const dot = document.querySelector('.dot');
 
-const click1 = new Audio('../audio/click2.mp3');
-const click2 = new Audio('../audio/click2.mp3');
-
+// const click1 = new Audio('../audio/click2.mp3');
+// const click2 = new Audio('../audio/click2.mp3');
+var click1 = new Audio('../audio/click2.mp3');
+click1.play()
+click1.pause()
 let bpm = 140;
 let beatsPerMeasure = 4;
 let count = 0;
@@ -37,7 +40,6 @@ socket.on('user_bpmeasure', function(msg) {
     beatsPerMeasure = parseInt(msg);
     updateMetronome();
 });
-
 
 function updateMetronome() {
     tempoDisplay.textContent = bpm;
@@ -70,8 +72,8 @@ function playClick() {
         click1.play();
         click1.currentTime = 0;
     } else {
-        click2.play();
-        click2.currentTime = 0;
+        click1.play(); //click 2
+        click1.currentTime = 0; //click 2
     }
     if (count%2 === 0){
       dot.style.background = "#fa545c";
@@ -82,4 +84,4 @@ function playClick() {
 }
 
 
-const metronome = new Timer(playClick, 60000 / bpm, { immediate: true });a
+const metronome = new Timer(playClick, 60000 / bpm, { immediate: true });
